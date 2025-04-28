dcecf5aab34530a6395fc082b4a0d360c6268959
maxim-gaiduchek <maxim.gayduchek@gmail.com>
2025-04-28 17:39:38 +0200

    feature: Init commit

---
+++ src/test/java/cz/cvut/fit/ejk/gaidumax/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package cz.cvut.fit.ejk.gaidumax;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
